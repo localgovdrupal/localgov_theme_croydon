c44a27c6ad3ca56fa81274839b6fcb658bcbc4a8
chris stevens <cjs78@hotmail.co.uk>
2021-07-21 16:44:55 +0100

    fixes relating to ditching the formio form from the footer feedback area

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,12 +25,12 @@
       }
     };
 
-    // open the feedback form in the footer if on the website-feedbak page
-    if (window.location.href.indexOf("website-feedback") > -1) {
-      setTimeout(function() { $('.reveal:first-child').click(); }, 100);
-      $('#sidebar-second').hide();
-      $('#content').hide();
-    }
+    // // open the feedback form in the footer if on the website-feedbak page
+    // if (window.location.href.indexOf("website-feedback") > -1) {
+    //   setTimeout(function() { $('.reveal:first-child').click(); }, 100);
+    //   $('#sidebar-second').hide();
+    //   $('#content').hide();
+    // }
     
     $('.page a').filter(function() {
       if(this.hostname.indexOf("croydon.gov.uk") === -1){
